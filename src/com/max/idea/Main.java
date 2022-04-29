8d84d45bef72f3043738e9376081ff79f6a4faac
Jegor <jegor.pakhutko@gmail.com>
2022-04-29 20:26:07 +0300

    task9

---
+++ src/com/max/idea/Main.java
@@ -0,0 +1,27 @@
+package com.max.idea;
+import java.util.Locale;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(in).useLocale(Locale.ENGLISH);;
+        System.out.println("Input array length");
+        int n = inp.nextInt();
+        if (n<1) {
+            System.out.println("Input number >0");
+            return;
+        }
+
+        double[] Arr;
+        Arr = new double[n];
+        int sum=0;
+        for (int i=0; i<n; i++)
+            sum+=i+1;
+
+        System.out.printf("The sum is equal to: %d\n", sum);
+    }
+}
